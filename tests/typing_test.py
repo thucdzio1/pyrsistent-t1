8f2e34e1ae65c7179f3457b4733759e91c3165d7
thucdzio1 <letienthuc2025@gmail.com>
2025-11-01 11:30:08 +0700

    sinh tesst

--- tests/typing_test.py
+++
@@ -1,6 +0,0 @@
-# Check that the inferred types are as expected.
-
-from typing_extensions import assert_type
-from pyrsistent import PMap, pmap
-
-assert_type(pmap(dict[tuple[int, str], float]()), PMap[tuple[int, str], float])
